84a9feddf14442f9e65c4cace0e55e7fa994504f
Sachin Jain <sacjain@adobe.com>
2016-03-01 22:33:39 +0530

    Requestly v4.1.0 released | Shared Rules feature

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
     zipup: {
       package: {
         appName: 'Requestly',
-        version: '4.0.1',
+        version: '4.1.0',
         files: [
           {
             cwd: 'src',
