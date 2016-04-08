aba4b9e8595723d2de1ba795ef0487633e6711eb
Sachin Jain <sacjain@adobe.com>
2016-04-08 21:00:12 +0530

    Requestly v4.1.1 released | Minor improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
     zipup: {
       package: {
         appName: 'Requestly',
-        version: '4.1.0',
+        version: '4.1.1',
         files: [
           {
             cwd: 'src',
